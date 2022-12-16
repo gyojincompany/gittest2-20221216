7918509f4baf9cf7fd93f97c8b90cb7ebb8e3488
gyojincompany <luckyred@gmail.com>
2022-12-16 16:52:25 +0900

    test

---
+++ src/main/java/com/shop/shopMallTest00/ShopMallTest00Application.java
@@ -0,0 +1,13 @@
+package com.shop.shopMallTest00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMallTest00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMallTest00Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/shopMallTest00/ShopMallTest00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shopMallTest00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMallTest00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
